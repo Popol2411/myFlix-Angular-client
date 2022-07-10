b35426080d08ef2d037bff72c91b58b2e0e0a9fa
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-07-10 14:37:28 +0200

    syntax correction

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -10,32 +10,6 @@ const apiUrl = 'https://myflixdbpopol.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
-  // Inject the HttpClient module to the constructor params
-  // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {
-  }
-  // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  private handleError(error: HttpErrorResponse): any {
-    if (error.error instanceof ErrorEvent) {
-      console.error('Some error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Error Status code ${error.status}, ` +
-        `Error body is: ${error.error}`);
-    }
-    return throwError(
-      'Something bad happened; please try again later.');
-  }
-}
-
 
 //User registration
 export class userRegistration {
@@ -72,11 +46,6 @@ export class userRegistration {
   }
 }
 
-
-// User login
-@Injectable({
-  providedIn: 'root',
-})
 export class UserLoginService {
   constructor(private http: HttpClient) { }
 
